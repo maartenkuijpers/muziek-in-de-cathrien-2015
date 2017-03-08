da380d450e7499d77861e278bf7e46912a92eb9b
maartenkuijpers <maarten@grasperk.nl>
2017-03-08 01:52:21 +0100

    Created separate MailChimp list.

--- midc-theme/page-nieuwsbrief.php
+++ midc-theme/page-nieuwsbrief.php
@@ -32,7 +32,7 @@ $email_address = $_POST["newsletter-email"];
         ?>
         <!-- Begin MailChimp Signup Form -->
         <div id="mc_embed_signup">
-            <form action="//collegiumvocaleeindhoven.us9.list-manage.com/subscribe/post?u=431a575944c9a01997ade9536&amp;id=9de492aac9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form-horizontal" target="_blank" novalidate>
+            <form action="//collegiumvocaleeindhoven.us9.list-manage.com/subscribe/post?u=431a575944c9a01997ade9536&amp;id=825bdfed78" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form-horizontal" target="_blank" novalidate>
                 <div id="mc_embed_signup_scroll">
                     <div class="form-group">
                         <label for="mce-EMAIL" class="col-sm-2 control-label">E-mail-adres*</label>
@@ -50,7 +50,6 @@ $email_address = $_POST["newsletter-email"];
                     <div class="form-group">
                         <div class="col-sm-2"></div>
                         <div class="col-sm-10">
-                            <p><a class="btn btn-info" role="button" href="http://us9.campaign-archive2.com/home/?u=431a575944c9a01997ade9536&id=9de492aac9" title="Bekijk eerdere maandbrieven">Bekijk eerdere maandbrieven</a></p>
                             <p class="help-block">* verplicht veld</p>
                         </div>
                     </div>
@@ -61,7 +60,7 @@ $email_address = $_POST["newsletter-email"];
                                 <div id="mce-error-response" style="display:none"></div>
                                 <div id="mce-success-response" style="display:none"></div>
                             </div><!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_431a575944c9a01997ade9536_9de492aac9" tabindex="-1" value=""></div>
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_431a575944c9a01997ade9536_825bdfed78" tabindex="-1" value=""></div>
                             <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-lg btn-success"><i class='fa fa-flag fa-2x pull-left'></i>Meld mij aan voor de maandbrief!</button>
                         </div>
                     </div>
